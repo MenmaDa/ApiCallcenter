ae8089706035ee631616772af0742559b027a3bb
Daniel Agudelo <dalfonsoagudelo@ucompensar.edu.co>
2026-05-22 11:54:01 -0500

    ApiCallcenter

--- database.py
+++ database.py
@@ -1,7 +1,18 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:12345@localhost:5432/callcenter"
+load_dotenv()
+
+DATABASE_URL = (
+    f"postgresql://{os.getenv('DB_USER')}:"
+    f"{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}:"
+    f"{os.getenv('DB_PORT')}/"
+    f"{os.getenv('DB_NAME')}"
+)
 
 engine = create_engine(DATABASE_URL)
 
